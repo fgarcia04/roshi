eb8852610ac28e14d48b8e25dc544f731ca94793
Federico Garcia <federico_garcia04@hotmail.com>
2021-12-11 12:59:23 -0300

    fix size text

--- resources/views/livewire/admin/home.blade.php
+++ resources/views/livewire/admin/home.blade.php
@@ -5,7 +5,7 @@
                 d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z"
                 clip-rule="evenodd"></path>
         </svg>
-        <h1 class="text-6xl font-semibold text-gray-700 dark:text-gray-200">
+        <h1 class="text-xl font-semibold text-gray-700 dark:text-gray-200">
             Proximamente...
         </h1>
         <p class="text-gray-700 dark:text-gray-300">
